75aa43f8454ca331427e754c5edf9f15d5eb4a1e
Engell25 <dark.engell.25@gmail.com>
2019-03-31 21:12:40 -0700

    Added string to variable

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 $(function() {
+    var number;
+    var numberstring ="";
+    var char;
     $('.button').on('mouseover', function() {
           $(this).css({background: '#1e1e1e'});
 
         $(this).click(function() {
             //$('.head').append($(this).attr("id"));
-            var char=$(this).text();
-            $('.head').append(char);
-            console.log(char);
+            
+            char=parseInt($(this).text());
+            if (isNaN(char)){
+                char=$(this).text();
+                console.log(char);
+
+            }else{
+                char=$(this).text();
+                
+                console.log(char);
+
+                
+                number = String(char);
+                numberstring = numberstring + number;
+                number = "";
+                char = "";
+            }
+            
+
+            
+
+            $('.head').html(numberstring);
+            console.log(numberstring);
         });
 
          /*$(this).one('click', function (event) {  
