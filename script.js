dc98f462ef338f4f0bd76ac6c19b3ec12d2d475c
Engell25 <dark.engell.25@gmail.com>
2019-04-01 01:12:12 -0700

    Updated jscript

--- script.js
+++ script.js
@@ -1,35 +1,59 @@
 $(function() {
-    var number;
+    var number = 0;
     var numberstring ="";
     var char;
+    var timer=0;
     $('.button').on('mouseover', function() {
-          $(this).css({background: '#1e1e1e'});
+        $(this).css({background: '#1e1e1e'});
 
         $(this).click(function() {
             //$('.head').append($(this).attr("id"));
-            
-            char=parseInt($(this).text());
-            if (isNaN(char)){
+            if (timer==1)
+            {
+                return false;
+            }
+            else
+            {
                 char=$(this).text();
-                console.log(char);
+                if (isNaN(parseInt(char)))
+                {
+                    console.log(char);
+                }
+                else
+                {
+                    //console.log(char);
 
-            }else{
-                char=$(this).text();
-                
-                console.log(char);
+                    if (number==0)
+                    {
+                        numberstring = char;
+                    }
+                    else
+                    {
+                        numberstring = numberstring + char;
+                    }
+                    
+                    char = 0;
+                    $('.head').html(numberstring);
+                    
 
+                }
+                console.log(number);
+                number++;
+                console.log(numberstring);
+                console.log(timer)
+
+                timer=1;
                 
-                number = String(char);
-                numberstring = numberstring + number;
-                number = "";
-                char = "";
+                    
+                    for (i = 0; i < 2000; i++) 
+                    {
+                        //console.log(i);
+                    }
+                    timer=0;
+                    console.log("Timer="+timer);
+                
             }
             
-
-            
-
-            $('.head').html(numberstring);
-            console.log(numberstring);
         });
 
          /*$(this).one('click', function (event) {  
@@ -53,3 +77,4 @@ function reply_click(clicked_id){
     $('.head').append(clicked_id);
 }
 */
+
