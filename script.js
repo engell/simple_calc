552c1af713003c6c31fb25efff677aebbc28c68a
Engell25 <dark.engell.25@gmail.com>
2019-04-17 09:00:16 -0700

    Debugged and added equation fields

--- script.js
+++ script.js
@@ -38,11 +38,14 @@ function keyclick(char)
             {
                 clear();
             }
-            if (char==".")
+            else if (char==".")
             {
-                append(char);
+                if (number % 1 == 0)
+                {
+                    append(char);
+                }
             }
-            if (char=="=")
+            else if (char=="=")
             {
                 result();
             }
@@ -61,62 +64,42 @@ function keyclick(char)
 
 function append(num)
 {
-    /*if (num==0)
+    if (num == 0 && numberstring == "0")
     {
-        numberstring = num;
-        /*
-        if (String(num) == "0")
-        {
-            numberstring = "0";
-            number = number - 1;
-        }
-        else
-        {
-            numberstring = String(num);
-            console.log("punto");
-        }
-    }
-    else
-    {
-        numberstring = numberstring + String(num);
         
-    }*/
-    //
-
-    if (isNaN(parseFloat(numberstring)))
-    {
-        console.log("esto no se va a ver nunca!!!");
         numberstring = "0";
         number = parseFloat(numberstring);
+        
     }
     else
     {
-        //numberstring = $('.head').text();
-        //numberstring = String(numberstring);
-        numberstring = numberstring + String(num);
-        //parseFloat(numberstring);
+        if (numberstring == "0")
+        {
+            numberstring = String(num);
+        }
+        else 
+        {
+            numberstring = numberstring + String(num);
+        }
         number = parseFloat(numberstring);
     }
-
-
-    $('.head').html(numberstring);
+    $('#head').html(numberstring);
     console.log(number);
-    
 }
 
 function clear()
 {
     number = 0;
     numberstring = "0";
-    $('.head').html(number);
+    $('#head').html(number);
 }
 
 function calc(t)
 {
     firstfactor = parseFloat(numberstring);
     equation = t;
-    //console.log(firstfactor);
-    //result();
+    $('#firstNumber').html(firstfactor);
+    $('#ecuation').html(equation);
     clear();
 }
 
@@ -128,21 +111,22 @@ function result()
     {
         numberstring = firstfactor + secondfactor;
     }
-    if (equation == "-")
+    else if (equation == "-")
     {
         numberstring = firstfactor - secondfactor;
     }
-    if (equation == "*")
+    else if (equation == "*")
     {
         numberstring = firstfactor * secondfactor;
     }
-    if (equation == "/")
+    else if (equation == "/")
     {
         numberstring = firstfactor / secondfactor;
     }
+    $('#secontNumber').html(secondfactor);
     firstfactor = 0;
     secondfactor = 0;
     equation = 0;
     number = 0;
-    $('.head').html(numberstring);
+    $('#head').html(numberstring);
 }
